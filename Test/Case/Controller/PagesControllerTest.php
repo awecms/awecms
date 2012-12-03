b7701870b8c4e14d060cf8d6dd6c25114d465684
David Gallagher <david@thegallagher.net>
2012-12-03 12:48:35 +1100

    Added blank PagesController test

---
+++ Test/Case/Controller/PagesControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+App::uses('PagesController', 'PieceOCake.Controller');
+
+/**
+ * PagesController Test Case
+ *
+ */
+class PagesControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.piece_o_cake.page'
+	);
+
+/**
+ * testAdminDisplay method
+ *
+ * @return void
+ */
+	public function testAdminDisplay() {
+	}
+
+}
