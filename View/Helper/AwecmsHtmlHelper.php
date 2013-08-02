d1eeeb7cbce5e9490c180d1228cfd3622a777b12
David Gallagher <david@thegallagher.net>
2013-08-02 14:15:41 +1000

    Allow uploaded images and media to be linked to easily.

--- View/Helper/AwecmsHtmlHelper.php
+++ View/Helper/AwecmsHtmlHelper.php
@@ -3,6 +3,22 @@
 App::uses('HtmlHelper', 'View/Helper');
 
 class AwecmsHtmlHelper extends HtmlHelper {
+    
+    public function image($path, $options = array()) {
+        if (!empty($options['upload'])) {
+            $options['pathPrefix'] = Configure::read('Awecms.uploadUrl');
+        }
+        unset($options['upload']);
+        return parent::image($path, $options);
+    }
+    
+    public function media($path, $options = array()) {
+        if (!empty($options['upload'])) {
+            $options['pathPrefix'] = Configure::read('Awecms.uploadUrl');
+        }
+
+        return parent::media($path, $options);
+    }
 
 	public function assetUrl($path, $options = array()) {
 		if (!empty($options['upload'])) {
