b92fb3e3a80ffa6a0971c546d56eaeeb9a58f9ca
David Gallagher <david@thegallagher.net>
2013-08-02 15:03:22 +1000

    Deprecated WidgetHelper::render() in favour of WidgetHelper::fetch(). Added WidgetHelper::fetchWidget() to fetch a single widget.

--- View/Helper/WidgetHelper.php
+++ View/Helper/WidgetHelper.php
@@ -28,7 +28,20 @@ class WidgetHelper extends Helper {
 		}
 	}
 
+
+	/**
+	 * Returns a rendered widget block.
+	 *
+	 * @param $blockName
+	 * @param array $options
+	 * @return string
+	 * @deprecated Use WidgetHelper::fetch() instead
+	 */
 	public function render($blockName, $options = array()) {
+		return $this->fetch($blockName, $options);
+	}
+
+	public function fetch($blockName, $options = array()) {
 		if (empty($options['renderer'])) {
 			$options['renderer'] = $this->settings['renderer'];
 		}
@@ -45,10 +58,38 @@ class WidgetHelper extends Helper {
 		
 		return $this->_View->element($options['renderer'], array('widgets' => $block, 'blockName' => $blockName, 'options' => $options));
 	}
+
+	public function fetchWidget($widgetId, $options = array()) {
+		if (empty($options['renderer'])) {
+			$options['renderer'] = $this->settings['renderer'];
+		}
+
+		if (!isset($options['class'])) {
+			$options['class'] = 'single-widget-' . $widgetId;
+		} else if ($options['class'] === false) {
+			$options['class'] = '';
+		}
+
+		$blockName = '_single_widget_' . $widgetId;
+		$block = $this->_initWidget($widgetId, $blockName);
+		if ($block === false) {
+			return false;
+		}
+
+		return $this->_View->element($options['renderer'], array('widgets' => $block, 'blockName' => $blockName, 'options' => $options));
+	}
 	
 	protected function _initBlock($blockName) {
 		if (!array_key_exists($blockName, $this->_blocks)) {
 			$this->_blocks[$blockName] = $this->_Widget->getWidgets($blockName);
+			$this->_call($blockName, 'initialize', array($this->_View));
+		}
+		return $this->_blocks[$blockName];
+	}
+
+	protected function _initWidget($widgetId, $blockName) {
+		if (!array_key_exists($blockName, $this->_blocks)) {
+			$this->_blocks[$blockName] = array($this->_Widget->getWidget($widgetId));
 			$this->_call($blockName, 'initialize', array($this->_View));
 		}
 		return $this->_blocks[$blockName];
