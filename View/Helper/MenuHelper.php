1f7699b34a420541d5dd8ddb200fe5a6274da034
David Gallagher <david@thegallagher.net>
2014-09-22 15:33:32 +1000

    Few minor changes, added documentation

--- View/Helper/MenuHelper.php
+++ View/Helper/MenuHelper.php
@@ -3,35 +3,67 @@
 App::uses('CakeEventManager', 'Event');
 App::uses('CakeEvent', 'Event');
 
+/**
+ * Class MenuHelper
+ *
+ * @property HtmlHelper $Html
+ */
 class MenuHelper extends AppHelper {
-	
-	public $helpers = array('Html');
-	
-	protected $_menuItems = array();
-	protected $_matchUrl = array();
-	protected $_rendering = array();
-	
-	public $settings = array();
-	
-	public function __construct(View $View, $settings = array()) {
-		parent::__construct($View, $settings);
-		$this->settings = Hash::normalize($settings);
-		
-		if (isset($this->settings['match'])) {
-			foreach ($this->settings['match'] as $group => $url) {
-				$this->match($url, $group);
-			}
-		}
-	}
-	
-	public function render($group = 'default', $options = array()) {
+
+/**
+ * Helpers
+ *
+ * @var array
+ */
+	public $helpers = ['Html'];
+
+/**
+ * Menu items
+ *
+ * @var array
+ */
+	protected $_menuItems = [];
+
+/**
+ * The menu items which are active in the current request
+ *
+ * @var array
+ */
+	protected $_matchUrl = [];
+
+/**
+ * Prevents recursion in sub menus
+ *
+ * @var array
+ */
+	protected $_rendering = [];
+
+/**
+ * Helper settings
+ *
+ * @var array
+ */
+	public $settings = [];
+
+/**
+ * Render a menu
+ *
+ * ### Options
+ *
+ * - `element` The element which will render the menu
+ *
+ * @param string $group Then menu group to render
+ * @param string|array $options Menu rendering options or if a string sets the element option
+ * @return null|string
+ */
+	public function render($group = 'default', $options = []) {
 		if (!empty($this->_rendering[$group])) {
 			return null;
 		}
 		$this->_rendering[$group] = true;
 		
 		if (is_string($options)) {
-			$options = array('element' => $options);
+			$options = ['element' => $options];
 		}
 		
 		$tmpVars = $options;
@@ -39,7 +71,7 @@ class MenuHelper extends AppHelper {
 		
 		$event = new CakeEvent('Menu.beforeRender', $this, $tmpVars);
 		CakeEventManager::instance()->dispatch($event);
-		if ($event->isStopped() || empty($this->_menuItems[$group])) {
+		if ($event->isStopped() || !$this->hasItems($group)) {
 			return null;
 		}
 		$tmpVars = $event->data;
@@ -55,10 +87,25 @@ class MenuHelper extends AppHelper {
 		$this->_rendering[$group] = false;
 		return $html;
 	}
-	
-	public function addItem($text, $url = null, $options = array()) {
+
+/**
+ * Add an item to a menu to be rendered later.
+ *
+ * Has the same options as `HtmlHelper::link()`
+ *
+ * ### Additional Options
+ *
+ * - `group` The menu group to place the menu item in.
+ * - `priority` The place in the menu to add the item. Defaults to 99 + (0.01 * count(menu items))
+ *
+ * @param string $title The content to be wrapped by <a> tags.
+ * @param string|array $url Cake-relative URL or array of URL parameters, or external URL (starts with http://)
+ * @param array $options Array of options and HTML attributes.
+ * @return void
+ */
+	public function item($title, $url = null, $options = []) {
 		if (is_string($options)) {
-			$options = array('group' => $options);
+			$options = ['group' => $options];
 		}
 		
 		$group = 'default';
@@ -69,22 +116,47 @@ class MenuHelper extends AppHelper {
 		if (isset($options['priority'])) {
 			$priority = $options['priority'];
 		}
-		if (!isset($this->_menuItems[$group])) {
-			$this->_menuItems[$group] = array();
+		if (!$this->hasItems($group)) {
+			$this->_menuItems[$group] = [];
 		}
 		$priority += count($this->_menuItems[$group]) * 0.01;
 		unset($options['group'], $options['priority']);
-		
-		$key = Router::normalize($url);
-		$this->_menuItems[$group][$key] = compact('text', 'url', 'options', 'priority');
+
+		if ($url === false) {
+			$key = 'item-' . count($this->_menuItems[$group]);
+		} else {
+			$key = Router::normalize($url);
+		}
+		$this->_menuItems[$group][$key] = compact('title', 'url', 'options', 'priority');
 	}
-	
+
+/**
+ * Remove an item from a menu
+ *
+ * @param string|array $url A URL to remove from the menu
+ * @param string $group The menu group to remove the the URL from
+ */
 	public function removeItem($url, $group = 'default') {
 		$key = Router::normalize($url);
 		unset($this->_menuItems[$group][$key]);
 	}
-	
-	public function link($text, $url = null, $options = array(), $confirmMessage = false) {
+
+/**
+ * Renders a menu link
+ *
+ * Has the same options as `HtmlHelper::link()`
+ *
+ * ### Additional Options
+ *
+ * - `wrap` An element to wrap the menu link in. Usually a <li> but defaults to none.
+ *    If `wrap` is set, class goes on wrapper instead of <a>.
+ *
+ * @param string $title The content to be wrapped by <a> tags.
+ * @param string|array $url Cake-relative URL or array of URL parameters, or external URL (starts with http://)
+ * @param array $options Array of options and HTML attributes.
+ * @return mixed A menu link
+ */
+	public function link($title, $url = null, $options = []) {
 		if ($this->isActive($url, $options)) {
 			$options['class'] = !empty($options['class']) ? $options['class'] . ' active' : 'active';
 		}
@@ -92,50 +164,71 @@ class MenuHelper extends AppHelper {
 		
 		if (isset($options['wrap'])) {
 			$wrap = $options['wrap'];
+			unset($options['wrap']);
+			if ($url === false) {
+				return $this->Html->tag($wrap, $title, $options);
+			}
+
 			$class = isset($options['class']) ? $options['class'] : null;
-			unset($options['wrap'], $options['class']);
-			$link = $this->Html->link($text, $url, $options, $confirmMessage);
-			return $this->Html->tag($wrap, $link, $class);
+			unset($options['class']);
+			return $this->Html->tag($wrap, $this->Html->link($title, $url, $options), ['class' => $class]);
 		} else {
-			return $this->Html->link($text, $url, $options, $confirmMessage);
+			if ($url === false) {
+				return $this->Html->tag('span', $title, $options);
+			}
+			return $this->Html->link($title, $url, $options);
 		}
 	}
-	
-	public function isActive($checkUrl, $options = array()) {
+
+/**
+ * Checks if a URL is currently active
+ *
+ * ### Options
+ *
+ * - `group` The menu group to check this URL
+ *
+ * @param string|array $url The URL to check
+ * @param array $options Array of options
+ * @return bool True if menu item is active
+ */
+	public function isActive($url, $options = []) {
+		if ($url === false) {
+			return false;
+		}
 		if (is_string($options)) {
-			$options = array('group' => $options);
+			$options = ['group' => $options];
 		}
 		$group = 'default';
 		if (isset($options['group'])) {
 			$group = $options['group'];
 		}
-		
+
 		if (!isset($this->_matchUrl[$group])) {
 			$this->match(Router::url(), $group);
 		}
-		
-		if (in_array(Router::normalize($checkUrl), $this->_matchUrl[$group])) {
-			return true;
-		}
-		
-		if (!empty($options['submenu']) && !empty($this->_menuItems[$options['submenu']])) {
-			foreach ($this->_menuItems[$options['submenu']] as $item) {
-				if ($this->isActive($item['url'], $item['options'])) {
-					return true;
-				}
-			}
-		}
-		return false;
+		return in_array(Router::normalize($url), $this->_matchUrl[$group]);
 	}
-	
-	public function match($url, $group = null) {
-		if ($group === null) {
-			$group = 'default';
-		}
+
+/**
+ * Make the current request match a URL in the menu.
+ *
+ * Use this method to make a menu item active.
+ *
+ * @param string|array $url The menu item to make active
+ * @param string $group The menu group to make the item active in
+ */
+	public function match($url, $group = 'default') {
 		$this->_matchUrl[$group][] = Router::normalize($url);
 	}
-	
+
+/**
+ * Check if a menu has menu items
+ *
+ * @param string $group Them menu group to check
+ * @return bool True if the menu has items
+ */
 	public function hasItems($group = 'default') {
 		return !empty($this->_menuItems[$group]);
 	}
+
 }
